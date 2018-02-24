ac1ee8c9172f2799ee3f9c6832855643712e3504
3ddesign <3ddesign@bk.ru>
2018-02-24 14:26:04 +0200

    test apps

---
+++ binding/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <app-quote>
+    <h2>{{ quoteTitle }}</h2>
+    <p>A wonderful Quote</p>
+  </app-quote>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue'
+
+  export default {
+    data: function () {
+      return {
+        quoteTitle: 'The Quote'
+      }
+    },
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ start/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!--<app-server-status v-for="server in 3"></app-server-status>-->
+    <app-server-status></app-server-status>
+  </div>
+</template>
+
+
+<script>
+  import ServerStatus from './ServerStatus.vue';
+
+  export default {
+    components: {
+      'app-server-status': ServerStatus
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  div {
+    border: 0.5px solid lightblue;
+  }
+</style>
